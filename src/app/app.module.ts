3e43172c1d3bc119d7866d4c7d4fdc3905109d72
Jorge Medrano <doblesim.es@gmail.com>
2020-05-09 10:40:02 +0200

    Creada la estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './pages/about/about.component';
+import { LoginComponent } from './login/login.component';
+
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PogressComponent } from './pages/pogress/pogress.component';
+import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcurmsComponent } from './shared/breadcurms/breadcurms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    PogressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcurmsComponent
   ],
   imports: [
     BrowserModule,
