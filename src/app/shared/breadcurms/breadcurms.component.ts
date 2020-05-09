d5f35920cf31ebff07f4fe1fbc1a55c22868e226
Jorge Medrano <doblesim.es@gmail.com>
2020-05-09 10:41:38 +0200

    Creada la estructura del proyecto

---
+++ src/app/shared/breadcurms/breadcurms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcurms',
+  templateUrl: './breadcurms.component.html',
+  styles: []
+})
+export class BreadcurmsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
